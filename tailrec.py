c19cfc543dfc605c35686ffb1d4ed6eb9835035f
Anthony Cipriano <anthony.nc@web.de>
2018-08-11 19:22:54 +0200

    Create tailrec.py

---
+++ tailrec.py
@@ -0,0 +1,24 @@
+class Tailrec:
+    def __init__(self, oper):
+        if isinstance(oper, Tailrec):
+            self.func = oper.func
+        else:
+            self.func = oper
+    def __call__(self, *args, **kwargs):
+        tailpos = self.func(*args, **kwargs)
+        while isinstance(tailpos, Tailcall):
+            tailpos = tailpos.evaluate()
+        return tailpos
+    def recur(self, *args, **kwargs):
+        return Tailcall(self.func, args, kwargs)
+
+class Tailcall:
+    def __init__(self, oper, args, kwargs):
+        if isinstance(oper, Tailrec):
+            self.func = oper.func
+        else:
+            self.func = oper
+        self.args = args
+        self.kwargs = kwargs
+    def evaluate(self):
+        return self.func(*self.args, **self.kwargs)

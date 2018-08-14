c6871e992bcec099782122fef36f4de20be9bb47
nayyarv <nayyarv@gmail.com>
2018-08-14 23:44:16 +1000

    Some style and doc stuff

--- asttailrec.py
+++ asttailrec.py
@@ -1,55 +1,65 @@
 """
 Tail recursion should be as simple as a decorator
-A basic implementation here
+
+This uses the AST of python to do the rewriting to reduce to existing Tailrec implementation
 """
 import ast
 
-
 from inspect import getsource, isclass
 from textwrap import dedent
-from itertools import takewhile
 
 from tailrec import Tailrec, Tailcall
 
-__all__ = ["ASTTailRec"]
+__all__ = ["ASTTailrec"]
+
 
 class TailTransformer(ast.NodeTransformer):
     """
-    Replace all the return statements we see that are just function calls
-    to the same function
+    Replace all the return statements we see that are just recursive function calls
+    to the same name with `name.recur`
     """
+
     def __init__(self, name, *args, **kwargs):
         self.name = name
         super(TailTransformer).__init__(*args, **kwargs)
 
     def visit_Return(self, node):
-        print(ast.dump(node))
         if isinstance(node.value, ast.Call):
             if node.value.func.id == self.name:
-                ctx = node.value.func.ctx
-                # node.value.func.attr = "recur"
+                # Since the dot syntax means that this an attribute call and not a function call, 
+                # we have to create a new node.
                 node.value.func = ast.Attribute(value=node.value.func, attr="recur", ctx=ast.Load())
+        # creating a new node doesn't always play nice with compile,
+        # so fix missing locations automatically
         ast.fix_missing_locations(node)
-        print(ast.dump(node))
         return node
 
 
 def replace_decorator(func_tree):
     """
-    The ASTTailRec decorator wants to get rid of itself and replace
-    with Tailrec. 
+    The ASTTailrec decorator wants to get rid of itself and replace with Tailrec so we can
+    reduce this problem to already solved issue.
+
     """
     decorators = func_tree.body[0].decorator_list
     for dec in decorators:
-        if dec.id == 'ASTTailRec':
+        if dec.id == 'ASTTailrec':
             dec.id = 'Tailrec'
 
 
-def ASTTailRec(func):
+def ASTTailrec(func):
     """
-    This approach involves modifying the ast tree and is heavily inspired by 
-    Robin Hillard's pipeop library at https://github.com/robinhilliard/pipes
-    which was used as a reference when developing this decorator
+    This approach involves modifying the ast tree so we can just stick a decorator on such as
+
+    ```
+    @ASTTailrec
+    def fac(n, k=1):
+        if n == 1: return k
+        return fac(n-1, k*n)
+    ```
+
+    This function has been heavily inspired by  Robin Hillard's pipeop library at
+    https://github.com/robinhilliard/pipes. It was used as reference when developing this decorator
     """
     if isclass(func):
         raise TypeError("Cannot apply tail recursion to a class")
@@ -57,42 +67,36 @@ def ASTTailRec(func):
     in_context = func.__globals__
 
     new_context = {"Tailrec": Tailrec, "Tailcall": Tailcall}
-    # these need to be included in the imports else we're gonna have some trouble
-    # if they've already been imported, let that import hold precendece.
-    new_context.update(in_context)
 
+    # these need to be included in the imports else we're gonna have some trouble
+    # if they've already been imported, let that import hold precedence.
+    new_context.update(in_context)
 
     # now let's try and get the source
     source = getsource(func)
     # we get the tree
     tree = ast.parse(dedent(source))
-    # print(ast.dump(tree))
-
 
     # update for debugger
     first_line_number = func.__code__.co_firstlineno
     ast.increment_lineno(tree, first_line_number - 1)
-    source_indent = sum([1 for _ in takewhile(str.isspace, source)]) + 1
 
-    
-    # let's grab the name of the function here
+    # let's grab the name of the function here. func.__name__ is not reliable in case
+    # of other decorators and no use of `functools.wraps`
     func_name = tree.body[0].name
 
     # we want to replace with the standard tailrec decorator here
     replace_decorator(tree)
 
-    # now everytime we find the function, let's replace with func_name.recur
+    # now every time we find the function, let's replace with func_name.recur
     # as in the standard case
     tree = TailTransformer(func_name).visit(tree)
-    # print(ast.dump(tree))
 
     # now the tree has been modified satisfactorily, let's compile
     code = compile(tree, filename=new_context['__file__'], mode='exec')
 
-    # exec
+    # exec the code in the scope of the new_context
     exec(code, new_context)
 
     # and return the function
-    # print(ast.dump(tree))
     return new_context[func_name]
-
--- test_asttail.py
+++ test_asttail.py
@@ -1,21 +1,24 @@
 #!/usr/bin/py.test
 
-from asttailrec import ASTTailRec
+from asttailrec import ASTTailrec
 import pytest
 
-@ASTTailRec
+
+@ASTTailrec
 def add_recur(a, b):
     if a <= 0: return b
-    return add_recur(a-1, b+1)
+    return add_recur(a - 1, b + 1)
+
 
 def add(a, b):
     if a <= 0: return b
-    return add(a-1, b+1)
+    return add(a - 1, b + 1)
 
 
 def test_recurse_error():
     with pytest.raises(RecursionError):
         add(1e5, 0)
+
 
 def test_no_recurse_error():
     add_recur(1e5, 0)
